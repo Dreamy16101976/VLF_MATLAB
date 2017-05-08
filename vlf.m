0df18e06876ec3113a4b06da0dd0a74b49b2b531
Alexey V. Voronin @ FoxyLab <Dreamy16101976@users.noreply.github.com>
2017-05-08 12:12:09 +0300

    Update vlf.m

--- vlf.m
+++ vlf.m
@@ -1,7 +1,7 @@
 % VLF spectrum analyzer in MATLAB
 % Version 0.1
 % license GPL v3.0
-% Alexey V. Voronin @ FoxyLab © 2017
+% Alexey V. Voronin @ FoxyLab Â© 2017
 % Email: support@foxylab.com
 % Website: https://acdc.foxylab.com
 % -----------------------------------
@@ -9,7 +9,7 @@
 clc; % cmd wnd clear
 close all;  % fig del
 disp('***** VLF spectrum analyzer *****');
-disp('© 2017 Alexey V. Voronin @ FoxyLab');
+disp('Â© 2017 Alexey V. Voronin @ FoxyLab');
 disp('https://acdc.foxylab.com');
 disp('**************************');
 % high-pass filter cutoff frequency
@@ -34,11 +34,28 @@ answer = inputdlg(prompt,'Numbers (N)',1,defans);
 if ~status
     error('Incorrect value!');
 end;
-disp(sprintf('f = %d Hz',low_freq));
+prompt = {'Low Frequency (peak) (f)'};
+defans = {'17000'};
+answer = inputdlg(prompt,'Low Frequency (peak) (f), Hz',1,defans);
+[peak_low status] = str2num(answer{1});
+if ~status
+    error('Incorrect value!');
+end;
+prompt = {'High Frequency (peak) (f)'};
+defans = {'26500'};
+answer = inputdlg(prompt,'High Frequency (peak) (f), Hz',1,defans);
+[peak_high status] = str2num(answer{1});
+if ~status
+    error('Incorrect value!');
+end;
+disp(sprintf('f(filter) = %d Hz',low_freq));
 disp(sprintf('dt = %d msec',window));
 disp(sprintf('N = %d',nums));
+disp(sprintf('f(low) = %d Hz',peak_low));
+disp(sprintf('f(high) = %d Hz',peak_high));
 Fs = 96000; % sampling freq, Hz
 duration = window/1000; % measure interval, sec
+while (true)
 ai = analoginput('winsound'); 
 addchannel(ai,1); % HW ch add
 % ch 1 - mono
@@ -51,8 +68,8 @@ bins = []; % bin array create
 trigger(ai); % acquire start
 data = getdata(ai); % data read
 L = length(data); % data array size
-disp(sprintf('L = %d samples',L));
-disp(sprintf('dF = %d Hz/bin',Fs/L));
+% disp(sprintf('L = %d samples',L));
+% disp(sprintf('dF = %d Hz/bin',Fs/L));
 for m =1:1:L/2+1 % bin array clear
     bins(m) = 0;
 end;
@@ -70,25 +87,69 @@ while (count < nums) % acquire loop
         bins(m) = (bins(m)*count+P1(m))/(count+1);
     end;
     count = count+1;
-    disp(sprintf('#%d',count));   
+    % disp(sprintf('#%d',count));   
 end
 stop(ai); % acquire stop
 delete(ai); % analog input object delete
 clear ai; % analog input object clear
 f = Fs*(0:(L/2))/L; % freqs
 for m =1:1:L/2+1 % low pass filter
-    if (m*Fs/L < low_freq) 
+    if ((m-1)*Fs/L < low_freq) 
         bins(m) = 0;
     end;
 end;
+%file name
+formatOut = 'dd.mm.yyyy HH:MM'; % date time
+unique = datestr(now,formatOut);
+disp(unique);
+formatOut = 'yyyymmddHHMMSS'; % date time
+unique = datestr(now,formatOut);
+unique_txt = strcat(unique,'.txt'); % png-file save  
+unique_png = strcat(unique,'.png'); % png-file save  
+txt_file = fopen(unique_txt,'w'); % log file open
+fprintf(txt_file,'f = %d Hz\r\n',low_freq);
+fprintf(txt_file,'dt = %d msec\r\n',window);
+fprintf(txt_file,'N = %d\r\n',nums);
+% peak detection
+peak_thres = 100e-6; %peak level
+peak_width = 2; % peak width/2
+for m =peak_width+1:1:L/2+1-(peak_width+1) % low pass filter
+    peak=true;
+    %asc test
+    for k=m-peak_width:1:m-1
+        if (bins(k)>=bins(m)) %peak fail
+            peak = false;
+        end;
+    end;
+    %desc test
+    for k=m+1:1:m+peak_width
+        if (bins(k)>=bins(m)) %peak fail
+            peak = false;
+        end;
+    end;
+    %width test
+    if ((bins(m)-bins(m-peak_width))<peak_thres) || ((bins(m)-bins(m+peak_width))<peak_thres)
+        peak = false; %peak fail
+    end;
+    if (peak==true)
+        if (((m-1)*Fs/L)>=peak_low) && (((m-1)*Fs/L)<=peak_high)
+            peak_level = bins(m)-bins(m-peak_width);
+            if ((bins(m)-bins(m+peak_width))<peak_level)
+                peak_level = bins(m)-bins(m+peak_width);
+            end;
+            disp(sprintf('Peak: %d Level: %5.0fu',(m-1)*Fs/L,peak_level*1e6));
+            fprintf(txt_file,'Peak: %d ',(m-1)*Fs/L);
+            fprintf(txt_file,'Level: %5.0fm\r\n',peak_level*1e6);
+        end;
+    end;
+end;
+fclose(txt_file); % log file close
 h = figure(1);
 plot(f,bins); % spectrum
 grid on;
 title('Spectrum'); % plot title
 xlabel('f, Hz') % OX axis label
-formatOut = 'yyyymmddHHMMSS'; % date time
-unique = datestr(now,formatOut);
-unique_png = strcat(unique,'.png'); % png-file save  
 saveas(h, unique_png, 'png'); % plot save
 close(h); % plot close 
+end;
 clear all; % objects delete
